2809559aac133a5ddab2478d550ba777e4644190
tangchunlin <tangchunlin@tangchunlins-MacBook-Pro.local>
2018-01-31 18:48:04 +0800

    test controller

---
+++ application/controllers/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Author: virtual.tang@yingzt.com
+ * Date: 31/01/2018
+ * Time: 6:47 PM
+ */
+class Index_Controller extends Yaf_Controller_Abstract
+{
+    public function index()
+    {
+        echo "hello world";
+    }
+}
